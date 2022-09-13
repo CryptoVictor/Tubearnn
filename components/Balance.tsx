5d0c9d4ca6965a24d390473315b2dc1f253a15da
Victor Garcia <hdjdududydusu@gmail.com>
2022-09-12 21:47:58 -0300

    Add files via upload

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,23 +1,10 @@
 import React from "react";
-import { user_tokens } from "../backend/index.js";
 
 const Balance = () => {
 
-    let balance;
-
-    if(user_tokens==undefined){
-
-        balance = 0.00;
-
-    }else{
-
-        balance = user_tokens;
-    }
-    
-
   return (
         <div>
-            $SOL {balance}
+            $SOL 0.00
         </div>
     )
 };
