1ac617cb3ec9b903c18b7d81519538c0816dd12a
Victor Garcia <hdjdududydusu@gmail.com>
2022-09-22 08:36:02 -0300

    Update Body.tsx

--- components/Body.tsx
+++ components/Body.tsx
@@ -74,7 +74,7 @@ const Body = (props: {
       ) : props.videos ? (
         <div className="grid grid-cols-3 gap-6 pt-16 px-32 place-content-center">
           {props.videos.map((video: any) => (
-             <Link href={`/${video.id.videoId}`}>
+             <Link key={0} href={`/${video.id.videoId}`}>
               <div key={video.etag}>
                 <div className="each mb-10 m-2 shadow-lg border-gray-800 bg-gray-100 relative">
                   <img
