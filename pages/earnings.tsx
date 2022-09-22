41e9af23eceaa2361ee155735380b546d4230f97
Victor Garcia <hdjdududydusu@gmail.com>
2022-09-22 07:04:56 -0300

    Update earnings.tsx

--- pages/earnings.tsx
+++ pages/earnings.tsx
@@ -39,7 +39,9 @@ export default function Dashboard() {
               className="p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl"
             >
               <div className="text-3xl font-bold py-2 text-center">
-                <Balance/>
+                <div>
+                  $SOL 0.0
+                </div>
               </div>
               <button
                 type="submit"
